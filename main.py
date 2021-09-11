518100b2b34f556a134392f4fb8880d9c3c692d5
Olivier Drevet <odrevet@gmail.com>
2021-09-11 16:29:01 +0200

    check collision on move right

--- main.py
+++ main.py
@@ -26,7 +26,6 @@ camera = [resolution_screen[0] / 2, resolution_screen[1] / 2]
 while True:
     surface_screen.fill(bgcolor)
     level.draw(hero, camera, surface_screen)
-    [bl, br, tl, tr] = hero.get_coords()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -44,8 +43,16 @@ while True:
                     hero.x -= 1
             if event.key == pygame.K_RIGHT:
                 hero.direction = Direction.RIGHT
-                if math.ceil((hero.x + 1) / (CUBE_SIZE)) < level.size[0]:
-                    hero.x += 1
+                next_x = hero.x + 1
+                tr = next_x + hero.size, hero.y
+                br = next_x + hero.size, hero.y + hero.size
+                if (
+                    math.ceil(next_x / (CUBE_SIZE)) < level.size[0]
+                    and level.tile(tr[0] // CUBE_SIZE, (tr[1]) // CUBE_SIZE, 1) is None
+                    and level.tile((br[0] - 1) // CUBE_SIZE, (br[1] - 1) // CUBE_SIZE, 1)
+                    is None
+                ):
+                    hero.x = next_x
             if event.key == pygame.K_UP:
                 hero.direction = Direction.UP
                 if hero.y > 0:
@@ -53,10 +60,13 @@ while True:
             if event.key == pygame.K_DOWN:
                 hero.direction = Direction.DOWN
                 next_y = hero.y + 1
+                bl = hero.x, next_y + hero.size
+                br = hero.x + hero.size, next_y + hero.size
                 if (
                     math.ceil(next_y / (CUBE_SIZE)) < level.size[1]
-                    and level.tile(bl[0] // CUBE_SIZE, (bl[1] - 1 + 1) // CUBE_SIZE, 1) is None
-                    and level.tile((br[0] - 1) // CUBE_SIZE, (br[1] - 1 + 1) // CUBE_SIZE, 1) is None
+                    and level.tile(bl[0] // CUBE_SIZE, (bl[1]) // CUBE_SIZE, 1) is None
+                    and level.tile((br[0] - 1) // CUBE_SIZE, (br[1]) // CUBE_SIZE, 1)
+                    is None
                 ):
                     hero.y = next_y
             if event.key == pygame.K_SPACE:
@@ -70,6 +80,7 @@ while True:
             )
             surface_screen.blit(textsurface, (0, font_size * 0))
 
+            [bl, br, tl, tr] = hero.get_coords()
             textsurface = font.render(
                 f"bl {bl} br {br} tl {tl} tr {tr}",
                 False,
