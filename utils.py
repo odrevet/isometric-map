6b5fb0fe5d3abb96867ecbf5ffd9e057073ea12d
odrevet <odrevet@gmail.com>
2021-09-26 21:35:19 +0200

    to 1d coords

--- utils.py
+++ utils.py
@@ -7,3 +7,7 @@ def cartesian_to_isometric(coord):
 
 def to_2d_coords(index, width):
     return Point2d(index % width, index // width)
+
+
+def to_1d_coords(position, width):
+    return position.x + width * position.y
