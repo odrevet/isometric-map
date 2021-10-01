3fe19fe75d06c17892bbf8068af17d9e4f828555
Olivier Drevet <odrevet@gmail.com>
2021-10-01 20:36:33 +0200

    Game class

---
+++ game.py
@@ -0,0 +1,226 @@
+import math
+
+import pygame
+from pygame.locals import *
+
+from utils import *
+from cube import Cube
+from hero import Direction
+
+class Game:
+    def __init__(self) -> None:
+        self.level = None
+        self.drawables = []
+        self.hero = None
+        self.ui_manager = None
+        self.clock = pygame.time.Clock()
+
+    def add_drawable(self, drawable):
+        self.drawables.append(drawable)
+
+    def draw(self, camera, surface_screen):
+        # draw
+        surface_screen.fill((0, 0, 0))
+        self.level.draw(self.drawables, camera, surface_screen)
+
+        # debug
+        if __debug__:
+            debug_text = f"{self.level.size.x}:{self.level.size.y}:{self.level.size.z} {self.hero.position.x}:{self.hero.position.y}:{self.hero.position.z}"
+            self.debug_textbox.html_text = debug_text
+            self.debug_textbox.rebuild()
+            self.ui_manager.draw_ui(surface_screen)
+
+            # draw lines around hero
+            # get hero coords and find isometric locations
+            bl, br, tl, tr = list(
+                map(
+                    (lambda coord: cartesian_to_isometric(coord.list())),
+                    self.hero.get_coords(),
+                )
+            )
+
+            # adjust all points with camera and hero z position
+            points = list(
+                map(
+                    (
+                        lambda point: (
+                            point.x + camera.x,
+                            point.y + camera.y - self.hero.position.z + Cube.SIZE,
+                        )
+                    ),
+                    [bl, br, br, tr, tl, tr, tl, bl],
+                )
+            )
+
+            pygame.draw.lines(
+                surface_screen,
+                (255, 255, 255),
+                False,
+                points,
+            )
+
+    def hero_on_ground(self, coords):
+        [bl, br, tl, tr] = coords
+        return (
+            self.level.get_cube(
+                bl.x // Cube.SIZE,
+                bl.y // Cube.SIZE,
+                (self.hero.position.z - 1) // Cube.SIZE,
+            )
+            is not None
+            or self.level.get_cube(
+                br.x // Cube.SIZE,
+                br.y // Cube.SIZE,
+                (self.hero.position.z - 1) // Cube.SIZE,
+            )
+            is not None
+            or self.level.get_cube(
+                tl.x // Cube.SIZE,
+                tl.y // Cube.SIZE,
+                (self.hero.position.z - 1) // Cube.SIZE,
+            )
+            is not None
+            or self.level.get_cube(
+                tr.x // Cube.SIZE,
+                tr.y // Cube.SIZE,
+                (self.hero.position.z - 1) // Cube.SIZE,
+            )
+            is not None
+        )
+
+    def update(self):
+        time_delta = self.clock.tick(60) / 1000.0
+        [bl, br, tl, tr] = self.hero.get_coords()
+        self.hero.on_ground = self.hero_on_ground([bl, br, tl, tr])
+
+        for drawable in self.drawables:
+            drawable.update_zindex()
+
+        # events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_q:
+                    pygame.quit()
+                    quit()
+
+                if event.key == pygame.K_LEFT:
+                    self.hero.is_moving = True
+                    self.hero.direction = Direction.LEFT
+                if event.key == pygame.K_RIGHT:
+                    self.hero.is_moving = True
+                    self.hero.direction = Direction.RIGHT
+                if event.key == pygame.K_UP:
+                    self.hero.is_moving = True
+                    self.hero.direction = Direction.UP
+                if event.key == pygame.K_DOWN:
+                    self.hero.is_moving = True
+                    self.hero.direction = Direction.DOWN
+
+                if __debug__ and event.key == pygame.K_d:
+                    sorted_drawables = sorted(
+                        self.level.cubes + self.drawables,
+                        key=lambda drawable: drawable.zindex,
+                    )
+                    for drawable in sorted_drawables:
+                        print(drawable)
+
+                if event.key == pygame.K_SPACE:
+                    if self.hero.on_ground:
+                        self.hero.jump = True
+            elif event.type == KEYUP:
+                if event.key in [
+                    pygame.K_LEFT,
+                    pygame.K_RIGHT,
+                    pygame.K_UP,
+                    pygame.K_DOWN,
+                ]:
+                    self.hero.is_moving = False
+
+        # update hero location
+        if self.hero.is_moving:
+            if self.hero.direction == Direction.UP:
+                if (
+                    self.hero.position.y - 1 >= 0
+                    and self.level.get_cube(
+                        tl.x // Cube.SIZE,
+                        (tl.y - 1) // Cube.SIZE,
+                        self.hero.position.z // Cube.SIZE,
+                    )
+                    is None
+                    and self.level.get_cube(
+                        tr.x // Cube.SIZE,
+                        (tr.y - 1) // Cube.SIZE,
+                        self.hero.position.z // Cube.SIZE,
+                    )
+                    is None
+                ):
+                    self.hero.position.y -= 1
+            elif self.hero.direction == Direction.RIGHT:
+                if (
+                    math.ceil((self.hero.position.x + 1) / (Cube.SIZE)) < self.level.size.x
+                    and self.level.get_cube(
+                        (tr.x + 1) // Cube.SIZE,
+                        tr.y // Cube.SIZE,
+                        self.hero.position.z // Cube.SIZE,
+                    )
+                    is None
+                    and self.level.get_cube(
+                        (br.x + 1) // Cube.SIZE,
+                        br.y // Cube.SIZE,
+                        self.hero.position.z // Cube.SIZE,
+                    )
+                    is None
+                ):
+                    self.hero.position.x += 1
+            elif self.hero.direction == Direction.DOWN:
+                if (
+                    math.ceil((self.hero.position.y + 1) / (Cube.SIZE)) < self.level.size.y
+                    and self.level.get_cube(
+                        bl.x // Cube.SIZE,
+                        (bl.y + 1) // Cube.SIZE,
+                        self.hero.position.z // Cube.SIZE,
+                    )
+                    is None
+                    and self.level.get_cube(
+                        br.x // Cube.SIZE,
+                        (br.y + 1) // Cube.SIZE,
+                        self.hero.position.z // Cube.SIZE,
+                    )
+                    is None
+                ):
+                    self.hero.position.y += 1
+            elif self.hero.direction == Direction.LEFT:
+                if (
+                    self.hero.position.x - 1 >= 0
+                    and self.level.get_cube(
+                        (tl.x - 1) // Cube.SIZE,
+                        tl.y // Cube.SIZE,
+                        self.hero.position.z // Cube.SIZE,
+                    )
+                    is None
+                    and self.level.get_cube(
+                        (bl.x - 1) // Cube.SIZE,
+                        bl.y // Cube.SIZE,
+                        self.hero.position.z // Cube.SIZE,
+                    )
+                    is None
+                ):
+                    self.hero.position.x -= 1
+
+        # jump
+        if self.hero.jump == True:
+            if self.hero.jump_cur >= self.hero.jump_max:
+                self.hero.jump = False
+                self.hero.jump_cur = 0
+            else:
+                self.hero.position.z += 1
+                self.hero.jump_cur += 1
+
+        # gravity
+        if self.hero.jump == False and not self.hero.on_ground:
+            self.hero.position.z -= 1
+
+        self.ui_manager.update(time_delta)
--- main.py
+++ main.py
@@ -1,260 +1,72 @@
-import math
-
 import pygame
 from pygame.locals import *
 
 import pygame_gui
 from pygame_gui.elements.ui_text_box import UITextBox
 
-from hero import Direction, Hero
+from hero import Hero
 from pot import Pot
 from chest import Chest
 from gold import Gold
 from level import *
 from cube import Cube
 from point2d import Point2d
-
-
-def hero_on_ground(hero, level):
-    return (
-        level.get_cube(
-            bl.x // Cube.SIZE, bl.y // Cube.SIZE, (hero.position.z - 1) // Cube.SIZE
-        )
-        is not None
-        or level.get_cube(
-            br.x // Cube.SIZE, br.y // Cube.SIZE, (hero.position.z - 1) // Cube.SIZE
-        )
-        is not None
-        or level.get_cube(
-            tl.x // Cube.SIZE, tl.y // Cube.SIZE, (hero.position.z - 1) // Cube.SIZE
-        )
-        is not None
-        or level.get_cube(
-            tr.x // Cube.SIZE, tr.y // Cube.SIZE, (hero.position.z - 1) // Cube.SIZE
-        )
-        is not None
-    )
+from game import Game
 
 
 # init pygame
 pygame.init()
-bgcolor = (0, 0, 0)
 
 resolution_screen = Point2d(320, 240)
 resolution_window = Point2d(640, 480)
 surface_window = pygame.display.set_mode(resolution_window.list())
 surface_screen = pygame.Surface(resolution_screen.list())
 pygame.display.set_caption("Isometric map")
-clock = pygame.time.Clock()
 camera = Point2d(resolution_screen.x // 2, resolution_screen.y // 2)
 
+game = Game()
+
 # init GUI
-ui_manager = pygame_gui.UIManager(resolution_screen.list(), "data/themes/classic.json")
-debug_textbox = UITextBox(
+game.ui_manager = pygame_gui.UIManager(
+    resolution_screen.list(), "data/themes/classic.json"
+)
+game.debug_textbox = UITextBox(
     "",
     pygame.Rect((0, 0), (320, 35)),
-    manager=ui_manager,
+    manager=game.ui_manager,
     object_id="#debug_textbox",
 )
 
 # Music
-try:
-    pygame.mixer.music.load("res/music/overworld_2.mid")
-    pygame.mixer.music.play()
-except:
-    pass
+#try:
+#    pygame.mixer.music.load("res/music/overworld_2.mid")
+#    pygame.mixer.music.play()
+#except:
+#    pass
 
 # init level
-level = Level()
-level.read("data/level.map")
+game.level = Level()
+game.level.read("data/level.map")
 
 # init hero
 hero = Hero(z=Cube.SIZE)
+game.hero = hero 
 
 # create drawables
 pot = Pot(Cube.SIZE, Cube.SIZE, Cube.SIZE * 4)
 chest = Chest(Cube.SIZE * 2, 0, Cube.SIZE)
 gold = Gold(Cube.SIZE * 2, Cube.SIZE * 2, Cube.SIZE)
 
-# drawables
-drawables = []
-drawables.append(pot)
-drawables.append(chest)
-drawables.append(gold)
-drawables.append(hero)
+# add drawables
+game.add_drawable(pot)
+game.add_drawable(chest)
+game.add_drawable(gold)
+game.add_drawable(hero)
 
 while True:
-    # logic
-    time_delta = clock.tick(60) / 1000.0
-    [bl, br, tl, tr] = hero.get_coords()
-    hero.on_ground = hero_on_ground(hero, level)
+    game.update()
+    game.draw(camera, surface_screen)
 
-    for drawable in drawables:
-        drawable.update_zindex()
-
-    # events
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            quit()
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_q:
-                pygame.quit()
-                quit()
-
-            if event.key == pygame.K_LEFT:
-                hero.is_moving = True
-                hero.direction = Direction.LEFT
-            if event.key == pygame.K_RIGHT:
-                hero.is_moving = True
-                hero.direction = Direction.RIGHT
-            if event.key == pygame.K_UP:
-                hero.is_moving = True
-                hero.direction = Direction.UP
-            if event.key == pygame.K_DOWN:
-                hero.is_moving = True
-                hero.direction = Direction.DOWN
-            
-            if __debug__ and event.key == pygame.K_d:
-                sorted_drawables = sorted(level.cubes + drawables, key=lambda drawable: drawable.zindex)
-                for drawable in sorted_drawables:
-                    print(drawable)
-
-            if event.key == pygame.K_SPACE:
-                if hero.on_ground:
-                    hero.jump = True
-        elif event.type == KEYUP:
-            if event.key in [pygame.K_LEFT, pygame.K_RIGHT, pygame.K_UP, pygame.K_DOWN]:
-                hero.is_moving = False
-
-    # update hero location
-    if hero.is_moving:
-        if hero.direction == Direction.UP:
-            if (
-                hero.position.y - 1 >= 0
-                and level.get_cube(
-                    tl.x // Cube.SIZE,
-                    (tl.y - 1) // Cube.SIZE,
-                    hero.position.z // Cube.SIZE,
-                )
-                is None
-                and level.get_cube(
-                    tr.x // Cube.SIZE,
-                    (tr.y - 1) // Cube.SIZE,
-                    hero.position.z // Cube.SIZE,
-                )
-                is None
-            ):
-                hero.position.y -= 1
-        elif hero.direction == Direction.RIGHT:
-            if (
-                math.ceil((hero.position.x + 1) / (Cube.SIZE)) < level.size.x
-                and level.get_cube(
-                    (tr.x + 1) // Cube.SIZE,
-                    tr.y // Cube.SIZE,
-                    hero.position.z // Cube.SIZE,
-                )
-                is None
-                and level.get_cube(
-                    (br.x + 1) // Cube.SIZE,
-                    br.y // Cube.SIZE,
-                    hero.position.z // Cube.SIZE,
-                )
-                is None
-            ):
-                hero.position.x += 1
-        elif hero.direction == Direction.DOWN:
-            if (
-                math.ceil((hero.position.y + 1) / (Cube.SIZE)) < level.size.y
-                and level.get_cube(
-                    bl.x // Cube.SIZE,
-                    (bl.y + 1) // Cube.SIZE,
-                    hero.position.z // Cube.SIZE,
-                )
-                is None
-                and level.get_cube(
-                    br.x // Cube.SIZE,
-                    (br.y + 1) // Cube.SIZE,
-                    hero.position.z // Cube.SIZE,
-                )
-                is None
-            ):
-                hero.position.y += 1
-        elif hero.direction == Direction.LEFT:
-            if (
-                hero.position.x - 1 >= 0
-                and level.get_cube(
-                    (tl.x - 1) // Cube.SIZE,
-                    tl.y // Cube.SIZE,
-                    hero.position.z // Cube.SIZE,
-                )
-                is None
-                and level.get_cube(
-                    (bl.x - 1) // Cube.SIZE,
-                    bl.y // Cube.SIZE,
-                    hero.position.z // Cube.SIZE,
-                )
-                is None
-            ):
-                hero.position.x -= 1
-
-    # jump
-    if hero.jump == True:
-        if hero.jump_cur >= hero.jump_max:
-            hero.jump = False
-            hero.jump_cur = 0
-        else:
-            hero.position.z += 1
-            hero.jump_cur += 1
-
-    # gravity
-    if hero.jump == False and not hero.on_ground:
-        hero.position.z -= 1
-
-    # draw
-    surface_screen.fill(bgcolor)
-    level.draw(drawables, camera, surface_screen)
-
-    # debug
-    if __debug__:
-        debug_text = f"{level.size.x}:{level.size.y}:{level.size.z} {hero.position.x}:{hero.position.y}:{hero.position.z}"
-        debug_textbox.html_text = debug_text
-        debug_textbox.rebuild()
-        ui_manager.draw_ui(surface_screen)
-
-        # draw lines around hero
-        # get hero coords and find isometric locations
-        bl, br, tl, tr = list(
-            map(
-                (lambda coord: cartesian_to_isometric(coord.list())),
-                hero.get_coords(),
-            )
-        )
-
-        # adjust all points with camera and hero z position
-        points = list(
-            map(
-                (
-                    lambda point: (
-                        point.x + camera.x,
-                        point.y + camera.y - hero.position.z + Cube.SIZE,
-                    )
-                ),
-                [bl, br, br, tr, tl, tr, tl, bl],
-            )
-        )
-
-        pygame.draw.lines(
-            surface_screen,
-            (255, 255, 255),
-            False,
-            points,
-        )
-
-    ui_manager.update(time_delta)
-
-    # stretch display
     scaled_win = pygame.transform.scale(surface_screen, surface_window.get_size())
     surface_window.blit(scaled_win, (0, 0))
-
     pygame.display.update()
